ef51e6e799ad769ed761f3eded33b1140c24f07b
F3us2022 <arsenal0288@gmail.com>
2022-04-07 12:08:33 -0400

    v1.2 commit

--- Data Structures & Modern Operators/app.js
+++ Data Structures & Modern Operators/app.js
@@ -24,7 +24,7 @@ const restaurant = {
         }
     },
     orderDelivery: function ({ mainIndex, starterIndex, time, address }) { console.log(`Order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} to be delivered to ${address} at ${time}`) },
-    orederPasta: function (ing1, ing2, ing3) {
+    orderPasta: function (ing1, ing2, ing3) {
         console.log(`Here is your delicious pasta with ingredients ${ing1} , ${ing2} and ${ing3}`)
     },
     orderPizza: function (mainIngredient, ...otherIngredient) { console.log(`Here is your delicious pizza with ingredients ${mainIngredient} , ${otherIngredient}`) }
@@ -303,7 +303,122 @@ console.log(rest1.numGuest, rest2.numGuest);
 
 
 
-//**------------------------------------------------- CODING CHALLENGE  --------------------------------*/
+//**------------------------------------------------- CODING CHALLENGE #1 --------------------------------*/
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+GOOD LUCK 😀
+*/
+
+
+
+
+
+
+//**------------------------------------------------- THE FOR-OF LOOP  --------------------------------*/
+
+/*
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const items of menu) { 
+    console.log(items);
+}
+
+//To get the index of the element in FOR OF LOOP use 'entries'.
+
+for (const items of menu.entries()) { 
+    console.log(items);
+}
+
+
+for (const [i,ele] of menu.entries()) { 
+    console.log(`index ${i} and elements ${ele}`);
+}
+
+*/
+
+
+
+//**-------------------------------------------------2020 OPTIONAL CHAINING ?.  --------------------------------*/
+
+//OPTIONAL CHAINING works on the concept of TRUTHY/FALSY value.
+
+/*
+if (restaurant.openingHours && restaurant.openingHours.mon)
+    console.log(restaurant.openingHours.mon.open);
+
+
+//Without optional chaining
+    //console.log(restaurant.openingHours.mon.open);
+//Using OPTIONAL CHAINING doing the same condition as above
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.thu?.open);
+
+//USE CASE
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const ele of days) { 
+    const open = restaurant.openingHours[ele]?.open ?? 'closed';
+    console.log(`On ${ele},The restaurant opens at ${open}`);
+}
+
+//OPTIONAL CHAINING For METHODS
+
+console.log(restaurant.order?.(0, 1) ?? 'Method doesnt exist');
+*/
+
+
+
+
+
+
+
+//**------------------------------------------LOOPING OBJECT - KEYS,VALUES and ENTRIES  --------------------------------*/
+
+// PROPERTY NAMES = KEYS
+
+/*
+
+const properties = Object.keys(restaurant.openingHours);
+console.log(properties);
+
+let openStr = `We are open on these ${properties.length} days :`;
+
+for (const day of properties)
+    openStr += `${day},`;
+console.log(openStr);
+
+
+//PROPERTY VALUEs = VALUES
+
+const value = Object.values(restaurant.openingHours);
+console.log(value);
+
+
+//PROPERTY ENTRIES = ENTRIES
+
+const entry = Object.entries(restaurant.openingHours);
+console.log(entry);
+
+//USE CASE
+
+for (const [key, { open, close }] of entry) { 
+    console.log(`On ${key} we open at ${open} and closes at ${close}`);
+}
+
+*/
+
+//**------------------------------------------------- CODING CHALLENGE #2 --------------------------------*/
 
 /* 
 Let's continue with our football betting app!
@@ -368,22 +483,7 @@ const game = {
 
 
 
-
-//**------------------------------------------------- THE FOR-OF LOOP  --------------------------------*/
-
-
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-for (const items of menu) { 
-    console.log(items);
-}
-
-//To get the index of the element in FOR OF LOOP use 'entries'.
-
-for (const items of menu.entries()) { 
-    console.log(items);
-}
+///**------------------------------------------------------ SETS---------------------------------------------*/
 
 
-for (const [i,ele] of menu.entries()) { 
-    console.log(`index ${i} and elements ${ele}`);
-}
+
