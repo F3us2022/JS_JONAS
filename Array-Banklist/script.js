3ef5b44c9741e76be6caaa784175f60f7ebd38ce
F3us2022 <arsenal0288@gmail.com>
2022-04-12 14:34:08 -0400

    v1.2 commit

--- Array-Banklist/script.js
+++ Array-Banklist/script.js
@@ -61,17 +61,247 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//** */
+
+//**------------------------------------------- CREATING DOM ELEMENTS USING 'FOR EACH'------------------------------------ */
+
+const displayMovements = function (movement) {
+  containerMovements.innerHTML = '';
+  movement.forEach(function (mov, index) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    //  console.log(type);
+    const html = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+    //  console.log(html);
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+//displayMovements(account1.movements);
+
+//
+
+//
+
+//**------------------------------------------ COMPUTING USERNAME USING MAP FUNCTION ----------------------------------- */
+
+const user = 'Steven Thomas Williams';
+const userLower = user.toLowerCase().split(' ');
+console.log(userLower);
+const initials = userLower.map(function (val) {
+  return val[0];
+});
+const username = initials.join('');
+console.log(username);
+
+//
+//---------- The above code in a one liner
+//
+
+const userName = user
+  .toLowerCase()
+  .split(' ')
+  .map(function (value) {
+    return value[0];
+  })
+  .join('');
+console.log(userName);
+
+//
+//---------- Using the ARROW FUNCTION
+//
+
+const userNameArrow = user
+  .toLowerCase()
+  .split(' ')
+  .map(value => value[0])
+  .join('');
+console.log(userNameArrow);
+
+//
+//---------- Using above logic to create the UserName
+//
+
+const createsUsername = function (accnt) {
+  accnt.forEach(function (accnOwner) {
+    accnOwner.username = accnOwner.owner
+      .toLowerCase()
+      .split(' ')
+      .map(value => value[0])
+      .join('');
+  });
+};
+
+createsUsername(accounts);
+console.log(accounts);
+
+//
+
+//
+
+//**------------------------------------------ ADDING BALANCE USING THE REDUCE FUNCTION ----------------------------------- */
+
+const calDisplayBalance = function (movements) {
+  const balance = movements.reduce(function (acc, mov) {
+    return acc + mov;
+  }, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+
+// calDisplayBalance(account1.movements);
+
+//
+
+//
+
+//**------------------------------------------ CALL DISPLAY SUMMARY USING CHAINING ----------------------------------- */
+
+/*
+const calDisplaySummary = function (movements) {
+  const income = movements
+    .filter(function (val) {
+      return val > 0;
+    })
+    .reduce(function (acc, valFil) {
+      return acc + valFil;
+    }, 0);
+  labelSumIn.textContent = `${income}€`;
+
+  const out = movements
+    .filter(function (val) {
+      return val < 0;
+    })
+    .reduce(function (acc, valFil) {
+      return acc + valFil;
+    }, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(function (val) {
+      return val > 0;
+    })
+    .map(function (valFil) {
+      return (valFil * 1.2) / 100;
+    })
+    .filter(function (valMap) {
+      return valMap >= 1;
+    })
+    .reduce(function (acc, valFil2) {
+      return acc + valFil2;
+    }, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calDisplaySummary(account1.movements);
+
+*/
+
+//**************  DOING THE SAME ABOVE LOGIC BUT THIS TIME JUST PASSING THE WHOLE ACCOUNT
+
+const calDisplaySummary = function (accnt) {
+  const income = accnt.movements
+    .filter(function (val) {
+      return val > 0;
+    })
+    .reduce(function (acc, valFil) {
+      return acc + valFil;
+    }, 0);
+  labelSumIn.textContent = `${income}€`;
+
+  const out = accnt.movements
+    .filter(function (val) {
+      return val < 0;
+    })
+    .reduce(function (acc, valFil) {
+      return acc + valFil;
+    }, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = accnt.movements
+    .filter(function (val) {
+      return val > 0;
+    })
+    .map(function (valFil) {
+      return (valFil * accnt.interest) / 100;
+    })
+    .filter(function (valMap) {
+      return valMap >= 1;
+    })
+    .reduce(function (acc, valFil2) {
+      return acc + valFil2;
+    }, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+//
+
+//
+
+//**------------------------------------- EVENT HANDLERS -> IMPLEMENTING LOGIN ------------------------------------------- */
+
+//
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  //Prevent Form from submitting
+
+  e.preventDefault();
+
+  currentAccount = accounts.find(function (accnt) {
+    // console.log('btn clicked', inputLoginUsername.value, accnt.username);
+    // return accnt.username === inputLoginUsername.value;
+    return 1;
+  });
+
+  console.log(currentAccount);
+
+  if (Number(inputLoginPin.value) === currentAccount?.pin) {
+    console.log('account login');
+
+    //DISPLAY UI AND WELCOME MESSAGE
+
+    labelWelcome.textContent = `Welcome Back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+
+    //CLEAR INPUT FIELDS
+
+    inputLoginPin.value = inputLoginUsername.value = ' ';
+
+    //DISPLAY MOVEMENTS
+
+    displayMovements(currentAccount.movements);
+
+    //DISPLAY BALANCE
+
+    calDisplayBalance(currentAccount.movements);
+
+    //DISPLAY SUMMARY
+
+    calDisplaySummary(currentAccount);
+  }
+});
+//
+
+//
+
+//
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+/*const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+*/
 
 /////////////////////////////////////////////////
 
@@ -212,4 +442,135 @@ Object.entries(currObg).forEach(function ([keyName, value], key, array) {
   console.log(`${keyName}:${value}`);
 });
 
+*/
+
+//console.log(accounts);
+
+//** */
+
+//
+//
+//**--------------------- DATA TRANSFORMATION USING MAP,FILTER,REDUCE,FIND ---------------------------------- */
+//
+
+//
+/*
+//-------------**********************  Using MAP function  *********************
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const usdToEur = 1.1;
+const movementsUSD = movements.map(function (mov) {
+  return mov * usdToEur;
+});
+
+console.log(movementsUSD);
+
+//-------------Using For Of loop to do the same as MAP function
+
+const arrNew = [];
+for (const val of movements) {
+  arrNew.push(val * usdToEur);
+}
+console.log(arrNew);
+*/
+
+//
+
+//
+//----------------*******************  Using Filter Method  *******************
+//
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposit = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposit);
+
+//-------------Using For Of loop to do the same as FILTER function
+
+const arrNew = [];
+for (const ele of movements) {
+  if (ele > 0) {
+    arrNew.push(ele);
+  }
+}
+console.log(arrNew);
+*/
+//
+
+//
+//----------------**********************  Using REDUCE Method  *******************
+//
+
+/*
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const balance = movements.reduce(function (acc, val, i) {
+  console.log(`Interation ${i}:Accumulator Value ${acc}`);
+  return acc + val;
+}, 0);
+console.log(balance);
+
+//-------------Using For Of loop to do the same as REDUCE function
+
+let balanceNew = 0;
+for (const ele of movements) {
+  balanceNew += ele;
+}
+console.log(balanceNew);
+
+//------------ Using REDUCE FUNCTION tocalculate the MAXIMUM VALUE in the ARRAY
+
+const max = movements.reduce(function (acc, val) {
+  if (acc > val) return acc;
+  else return val;
+}, movements[0]);
+console.log(max);
+
+*/
+//
+
+//
+//----------------**********************  Using FIND Method  *******************
+//
+
+/*
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const firstWithdrawal = movements.find(function (val) {
+  return val < 0;
+});
+console.log(firstWithdrawal);
+
+//finding the username using FIND
+
+const accnt = accounts.find(function (accn) {
+  return accn.owner == 'Jessica Davis';
+});
+console.log(accnt);
+//
+
+*/
+
+//
+//
+//**----------------------------------------------------- CHAINING METHODS ----------------------------------------------- */
+
+//
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+const totalDepositeUSD = movements
+  .filter(function (val) {
+    return val > 0;
+  })
+  .map(function (valFil) {
+    return valFil * eurToUsd;
+  })
+  .reduce(function (acc, valMap) {
+    return acc + valMap;
+  }, 0);
+console.log(totalDepositeUSD);
 */
