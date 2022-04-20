537199436d5e44e90b4cc7d8ac29bfc60928935f
F3us2022 <arsenal0288@gmail.com>
2022-04-20 02:57:24 -0400

    v1.1 commit

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -29,3 +29,76 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//
+
+//
+
+//
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//
+
+//**------------------------------------ SELECTING / CREATING AND DELETING ELEMENTS -------------------------------------*/
+
+//
+
+//
+
+//--------*************** SELECTING ELEMENTS *******************--/
+
+const elem = document.querySelector('.header');
+console.log(elem);
+const allSelec = document.querySelectorAll('.section'); // not live thats why represented by NODES
+console.log(allSelec);
+const idSelec = document.getElementById('section--1');
+console.log(idSelec);
+const tagSelec = document.getElementsByTagName('button'); //Live elements thats why have collection of elements
+console.log(tagSelec);
+const classSelec = document.getElementsByClassName('btn'); //Live elements thats why have collection of elements
+console.log(classSelec);
+
+
+//---------************* CREATING ELEMENTS *********************--/
+
+// Method 1-- > .insertAdjacentHTML
+
+// Method 2-- >
+
+//--Create New Element
+const message = document.createElement('div');
+//Add attribute to the New Element
+message.classList.add('cookie-message');
+message.innerHTML=`We use cookie for improved functionality and analytic . <button class="btn btn--close-cookie">Got It!</button> `
+//Get the Element where the New element to be inserted
+const hdr = document.querySelector('.header');
+//add the new element -- (Several places to append the new element)
+//hdr.prepend(message);
+hdr.append(message);
+//hdr.append(message.cloneNode(true));
+//hdr.before(message);
+//hdr.after(message);
+
+//
+//---------************* DELETING ELEMENTS *********************--/
+//
+
+//--- Method --> 1
+//const clsBtn = document.querySelector('.btn--close-cookie');
+// console.log(clsBtn);
+// clsBtn.addEventListener('click', function () { 
+//   message.remove();
+// });
+
+
+//--- Method --> 2
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => message.parentElement.removeChild(message));
+
+//
+
+//
+
+//
+
